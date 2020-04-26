5daadd20dd8fe4e3e6ccf562a6863be5cdf7b910
Quantum Webgarden <58441029+quantumwebgarden@users.noreply.github.com>
2020-04-26 21:13:52 +0530

    Instagram Support File Uploaded

--- client3/themagicshoot/assets/tms/insta.js
+++ client3/themagicshoot/assets/tms/insta.js
@@ -11,7 +11,10 @@ var config = {
   };
   firebase.initializeApp(config);            
 var ntn = 0;
-
+var img = 0;
+var vdo = 0;
+var viewer = 0;
+var posts = 0;
 $(document).ready(function(){
 
 var rootRef = firebase.database().ref('instagram');
@@ -33,18 +36,17 @@ ntn = ntn +1;
 
 
 if(dtype == "Image"){
-$("#og-grid").append("<li><a href=\"#\" data-largesrc=\"" + ulnk + "\" data-title=\"" + insid + "\" data-description=\"" + cap + "\"><img src=\"" + ulnk + "\" width=\"auto\" height=\"245px\" alt=\"img" + ntn + "\" onclick=\"addField(\'" + insid +"\',\'" + ulnk +"\',\'" + uname +"\',\'" + cap +"\');\"/></a></li>");
+$("#galinsta").append("<div class=\"gallery-item\" tabindex=\"0\"><img src=\"" + ulnk + "\" class=\"gallery-image\" alt=\"\"><div class=\"gallery-item-type\"><span class=\"visually-hidden\">Gallery</span><i class=\"fas fa-image\" aria-hidden=\"true\"></i></div><div class=\"gallery-item-info\" onclick=\"imgfr(\'" + insid +"\',\'" + ulnk +"\',\'" + uname +"\',\'" + cap +"\');\"><ul><li class=\"gallery-item-likes\"><span class=\"visually-hidden\">Likes:</span><i class=\"fas fa-heart\" aria-hidden=\"true\"></i> 42</li><li class=\"gallery-item-comments\"><span class=\"visually-hidden\">Comments:</span><i class=\"fas fa-comment\" aria-hidden=\"true\"></i> 1</li></ul></div></div>");
 }
 if(dtype == "Video"){
-$("#og-grid").append("<li><a href=\"#\"><iframe style=\"width: auto; height: 245px;\" src=\"" + ulnk + "\" allowfullscreen></iframe></a></li>");
-
+$("#galinsta").append("<div class=\"gallery-item\" tabindex=\"0\"><iframe width=\"100%\" height=\"200\" src=\"" + ulnk + "\"  frameborder=\"0\" allowfullscreen></iframe><div class=\"gallery-item-type\"><span class=\"visually-hidden\">Gallery</span><i class=\"fas fa-video\" aria-hidden=\"true\"></i></div><div class=\"gallery-item-info\" onclick=\"vdofr(\'" + insid +"\',\'" + ulnk +"\',\'" + uname +"\',\'" + cap +"\');\"><ul><li class=\"gallery-item-likes\"><span class=\"visually-hidden\">Likes:</span><i class=\"fas fa-heart\" aria-hidden=\"true\"></i> 42</li><li class=\"gallery-item-comments\"><span class=\"visually-hidden\">Comments:</span><i class=\"fas fa-comment\" aria-hidden=\"true\"></i> 1</li></ul></div></div>");
 }
 });
 
 });
 
-
-function addField(insid,ulnk,uname,cap){
+/*
+function showimage(insid,ulnk,uname,cap){
   
   //var file = "https://firebasestorage.googleapis.com/v0/b/admission-2020.appspot.com/o/gallery%2Fg" + appid + "?alt=media&token=" + pht
   Swal.fire({
@@ -55,3 +57,69 @@ function addField(insid,ulnk,uname,cap){
   showConfirmButton: false,
 });
   }
+
+  function showvideo(insid,ulnk,uname,cap){
+  
+  //var file = "https://firebasestorage.googleapis.com/v0/b/admission-2020.appspot.com/o/gallery%2Fg" + appid + "?alt=media&token=" + pht
+  Swal.fire({
+  title: cap,
+  text: "Uploaded by" + uname + ".",
+  html:
+    'Uploaded by <b>' + uname + '</b>, ' +
+    '<iframe width="285" height="285" src="' + ulnk + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' +
+    'Instagram',
+  showConfirmButton: false,
+});
+  }
+*/
+function imgfr(insid,ulnk,uname,cap)
+  {
+    on1();
+  document.getElementById("imgshsrc1").src = ulnk;
+  document.getElementById("dtls1").innerHTML = "Uploaded by <b>" + uname+ "</b>";
+  document.getElementById("tdtls1").innerHTML = cap;
+  }
+  function vdofr(insid,ulnk,uname,cap)
+  {
+    on2();
+  document.getElementById("imgshsrc2").src = ulnk;
+  document.getElementById("dtls2").innerHTML = "Uploaded by <b>" + uname+ "</b>";
+  document.getElementById("tdtls2").innerHTML = cap;
+  
+  }
+
+  function on1() {
+  document.getElementById("overlay1").style.height = "100%";
+  document.getElementById("overlay1").style.width = "100%";
+  document.getElementById("overlay1").style.top = "0%";
+  document.getElementById("overlay1").style.left = "0%";
+  document.body.style.overflowY = 'hidden';
+
+}
+
+function off1() {
+   document.getElementById("overlay1").style.height = "0%";
+   document.getElementById("overlay1").style.width = "0%";
+   document.getElementById("overlay1").style.top = "50%";
+  document.getElementById("overlay1").style.left = "50%";
+  document.getElementById("imgshsrc1").src = "";
+  document.body.style.overflowY = 'auto';
+  
+}
+function on2() {
+  document.getElementById("overlay2").style.height = "100%";
+  document.getElementById("overlay2").style.width = "100%";
+  document.getElementById("overlay2").style.top = "0%";
+  document.getElementById("overlay2").style.left = "0%";
+  document.body.style.overflowY = 'hidden';
+}
+
+function off2() {
+   document.getElementById("overlay2").style.height = "0%";
+   document.getElementById("overlay2").style.width = "0%";
+   document.getElementById("overlay2").style.top = "50%";
+  document.getElementById("overlay2").style.left = "50%";
+  document.getElementById("imgshsrc2").src = "";
+  document.body.style.overflowY = 'auto';
+}
+
