54676d3cf2f7942e4e5eb452c2846600bb7bea58
Quantum Webgarden <58441029+quantumwebgarden@users.noreply.github.com>
2020-05-06 20:48:53 +0530

    Game Link Updated

--- game/blc2.js
+++ game/blc2.js
@@ -5,7 +5,7 @@
 	document.head.insertAdjacentHTML('beforeend','<style>.sg { width: 35px; height: 35px; position: fixed; bottom: 10px; right: 10px; } .sg .eye { -webkit-transform: translateX(0px);   transform: translateX(0px); } .sg:hover .eye { -webkit-transition: -webkit-transform 0.3s ease; transition: -webkit-transform 0.3s ease; transition: transform 0.3s ease; transition: transform 0.3s ease, -webkit-transform 0.3s ease; -webkit-transform: translateX(12px);   transform: translateX(12px); }</style>'); 
   
 	var a = document.createElement('a');
-	a.setAttribute('href','https://twitter.com/steeevg');
+	a.setAttribute('href','http://quantumwebgarden.in/');
 	a.setAttribute('target','_blank');
 	a.innerHTML = icon;
     
