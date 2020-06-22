1d94226f83bb92eb05760ae520e289e0a8e0f626
Quantum Webgarden <58441029+quantumwebgarden@users.noreply.github.com>
2020-06-22 09:14:18 -0700

    Updated

---
+++ client4/verify.js
@@ -0,0 +1,45 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCIHNdljOqzWgasMfB2bBZuFVHhof3-SLQ",
+    authDomain: "quantumdot20.firebaseapp.com",
+    databaseURL: "https://quantumdot20.firebaseio.com",
+    projectId: "quantumdot20",
+    storageBucket: "quantumdot20.appspot.com",
+    messagingSenderId: "1011845585600",
+    appId: "1:1011845585600:web:cdd2e4e8ee38046b262b25",
+    measurementId: "G-Y98ZR7S8EQ"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+window.onload=function () {
+  render();
+};
+function render() {
+    window.recaptchaVerifier=new firebase.auth.RecaptchaVerifier('recaptcha-container');
+    recaptchaVerifier.render();
+}
+function phoneAuth() {
+    //get the number
+    var number= "+91" + document.getElementById('number').value;
+    //phone number authentication function of firebase
+    //it takes two parameter first one is number,,,second one is recaptcha
+    firebase.auth().signInWithPhoneNumber(number,window.recaptchaVerifier).then(function (confirmationResult) {
+        //s is in lowercase
+        window.confirmationResult=confirmationResult;
+        coderesult=confirmationResult;
+        console.log(coderesult);
+        alert("Message sent");
+    }).catch(function (error) {
+        alert(error.message);
+    });
+}
+function codeverify() {
+    var code=document.getElementById('verificationCode').value;
+    coderesult.confirm(code).then(function (result) {
+        alert("Successfully registered");
+        var user=result.user;
+        window.AppInventor.setWebViewString(user + "uid");
+        console.log(user);
+    }).catch(function (error) {
+        alert(error.message);
+    });
+}
