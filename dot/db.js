18978b364144f6b371a1ebc95f313ab02a4461f8
Quantum Webgarden <58441029+quantumwebgarden@users.noreply.github.com>
2020-07-31 15:57:07 +0530

    updated

--- dot/db.js
+++ dot/db.js
@@ -244,6 +244,17 @@ function initskip() {
 }
 
   function loadtime(){
+
+    Swal.fire({
+      html: '<img src="assets/img/loading.gif"><br><b>Will be available at your service soon ...</b>',
+        allowEscapeKey: false,
+        allowOutsideClick: false,
+        showCancelButton: false,
+        showConfirmButton: false,
+        focusConfirm: false,
+        footer: '<p>DOT : Delivery On Time</p>'
+        });
+  /*
     var qtload = Math.floor(Math.random() * 4);
     let timerInterval
 Swal.fire({
@@ -276,12 +287,12 @@ Swal.fire({
     }
   }
 }).then((result) => {
-  /* Read more about handling dismissals below */
+  /* Read more about handling dismissals below 
   if (result.dismiss === Swal.DismissReason.timer && u.includes("Guest")) {
     initskip();
   }
 })
-  }
+  */}
 
 
 function foodshp(){
