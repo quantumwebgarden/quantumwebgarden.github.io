f71d026f3ac41cc584665e95a95c4a45cfaa41f1
Quantum Webgarden <58441029+quantumwebgarden@users.noreply.github.com>
2020-09-29 14:51:09 +0530

    Updated

--- dot/db.js
+++ dot/db.js
@@ -301,7 +301,7 @@ Swal.fire({
       document.getElementById("imgc1").src = "assets/banner/imgc3.jpg";
       document.getElementById("imgc2").src = "assets/banner/imgc2.jpg";
       document.getElementById("imgc3").src = "assets/banner/imgc1.gif";
-      document.getElementById("imgc4").src = "assets/banner/imgc3.png";
+      document.getElementById("imgc4").src = "assets/banner/imgc4.png";
 	    document.getElementById("imgc5").src = "assets/banner/imgc2.gif";
     }
   }
