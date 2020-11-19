0c6dde60ef2c626bf71641f0c1b621ee8fd4942a
Quantum Webgarden <58441029+quantumwebgarden@users.noreply.github.com>
2020-11-19 12:53:06 +0530

    Updated

--- dot/restpayadmin.js
+++ dot/restpayadmin.js
@@ -13,18 +13,18 @@ var mcshare = 0;
 var stocker = ["","checked"];
 var payst = ["Unpaid","Paid"];
 
-var firebaseConfig = {
-    apiKey: "AIzaSyA7wly3NvCF90pJFs7a9kMBl-eXHJvXYw4",
-    authDomain: "dotqwdtls.firebaseapp.com",
-    databaseURL: "https://dotqwdtls.firebaseio.com",
-    projectId: "dotqwdtls",
-    storageBucket: "dotqwdtls.appspot.com",
-    messagingSenderId: "374863944922",
-    appId: "1:374863944922:web:3025e1648144afe89fd9e3",
-    measurementId: "G-NWB5LL551K"
-  };
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();     
+// var firebaseConfig = {
+//     apiKey: "AIzaSyA7wly3NvCF90pJFs7a9kMBl-eXHJvXYw4",
+//     authDomain: "dotqwdtls.firebaseapp.com",
+//     databaseURL: "https://dotqwdtls.firebaseio.com",
+//     projectId: "dotqwdtls",
+//     storageBucket: "dotqwdtls.appspot.com",
+//     messagingSenderId: "374863944922",
+//     appId: "1:374863944922:web:3025e1648144afe89fd9e3",
+//     measurementId: "G-NWB5LL551K"
+//   };
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();     
 
 function getuid() {
     var parameters = location.search.substring(1).split("=");
