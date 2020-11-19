51ca4fa992bf9440a901b404c56648d0bcfdcfc8
Quantum Webgarden <58441029+quantumwebgarden@users.noreply.github.com>
2020-11-19 12:31:02 +0530

    Updated

--- dot/manager.js
+++ dot/manager.js
@@ -1,18 +1,18 @@
 var statusall = ["offline","sit","road"];
 var roadstatus = "";
 var stocker = ["","checked"];
-var firebaseConfig = {
-    apiKey: "AIzaSyA7wly3NvCF90pJFs7a9kMBl-eXHJvXYw4",
-    authDomain: "dotqwdtls.firebaseapp.com",
-    databaseURL: "https://dotqwdtls.firebaseio.com",
-    projectId: "dotqwdtls",
-    storageBucket: "dotqwdtls.appspot.com",
-    messagingSenderId: "374863944922",
-    appId: "1:374863944922:web:3025e1648144afe89fd9e3",
-    measurementId: "G-NWB5LL551K"
-  };
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();     
+// var firebaseConfig = {
+//     apiKey: "AIzaSyA7wly3NvCF90pJFs7a9kMBl-eXHJvXYw4",
+//     authDomain: "dotqwdtls.firebaseapp.com",
+//     databaseURL: "https://dotqwdtls.firebaseio.com",
+//     projectId: "dotqwdtls",
+//     storageBucket: "dotqwdtls.appspot.com",
+//     messagingSenderId: "374863944922",
+//     appId: "1:374863944922:web:3025e1648144afe89fd9e3",
+//     measurementId: "G-NWB5LL551K"
+//   };
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();     
 
 
 function deleteguest(){
