35d2c9597aef1ad7ec9902a4175f3699f9e54914
Quantum Webgarden <58441029+quantumwebgarden@users.noreply.github.com>
2020-11-19 10:01:05 +0530

    Updated

---
+++ dot/doturation.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA7wly3NvCF90pJFs7a9kMBl-eXHJvXYw4",
+    authDomain: "dotqwdtls.firebaseapp.com",
+    databaseURL: "https://dotqwdtls.firebaseio.com",
+    projectId: "dotqwdtls",
+    storageBucket: "dotqwdtls.appspot.com",
+    messagingSenderId: "374863944922",
+    appId: "1:374863944922:web:3025e1648144afe89fd9e3",
+    measurementId: "G-NWB5LL551K"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();     
