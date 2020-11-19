afaad983442e1f92eae6065c77aa1bf4c484782f
Quantum Webgarden <58441029+quantumwebgarden@users.noreply.github.com>
2020-11-19 12:47:54 +0530

    Updated

--- dot/reg.js
+++ dot/reg.js
@@ -21,17 +21,17 @@ function getuid() {
 	console.log("https://quantumwebgarden.github.io/dot/reg.html?uid=" + uid + "=0=" + lats + "=" + langs + "=0")
 }
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCIHNdljOqzWgasMfB2bBZuFVHhof3-SLQ",
-    authDomain: "quantumdot20.firebaseapp.com",
-    databaseURL: "https://quantumdot20.firebaseio.com",
-    projectId: "quantumdot20",
-    storageBucket: "quantumdot20.appspot.com",
-    messagingSenderId: "1011845585600",
-    appId: "1:1011845585600:web:cdd2e4e8ee38046b262b25",
-    measurementId: "G-Y98ZR7S8EQ"
-  };
-  firebase.initializeApp(firebaseConfig);
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCIHNdljOqzWgasMfB2bBZuFVHhof3-SLQ",
+//     authDomain: "quantumdot20.firebaseapp.com",
+//     databaseURL: "https://quantumdot20.firebaseio.com",
+//     projectId: "quantumdot20",
+//     storageBucket: "quantumdot20.appspot.com",
+//     messagingSenderId: "1011845585600",
+//     appId: "1:1011845585600:web:cdd2e4e8ee38046b262b25",
+//     measurementId: "G-Y98ZR7S8EQ"
+//   };
+//   firebase.initializeApp(firebaseConfig);
 
 function checkreg(){
 	
