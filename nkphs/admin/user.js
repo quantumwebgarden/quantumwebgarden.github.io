5b2d119fa313b0e3af7e7fac407ce5c0a60c9c9e
Quantum Webgarden <58441029+quantumwebgarden@users.noreply.github.com>
2021-01-21 19:17:29 +0530

    Updated

--- nkphs/admin/user.js
+++ nkphs/admin/user.js
@@ -5,7 +5,7 @@ var cls = "";
 var cnt = 0;
 function fldtls() {
     $("#table_body").empty();
-    //fixedData();
+    adminUpdate();
     slctupd();
     allData();
     }        
@@ -86,7 +86,7 @@ else{
 }
 function getAllBook(x){
 var cnt = 0;
-
+var totalprice = 0;
 var y = document.getElementById("clsslct").value;
 $("#" + x + "_body").empty();
 var rootRef = firebase.database().ref("item/package/"+ x + "/" + y + "/books");
@@ -97,6 +97,7 @@ var sub = snap.child("sub").val();
 var name = snap.child("name").val();
 var price = snap.child("price").val();
 var quantity = snap.child("quantity").val();
+totalprice = totalprice + Number(price);
 
 var removebtn = '<button onclick="removeBook(\'' + id + '\',\'' + x + '\',\'' + y + '\')" class="button">Delete Book</button>';
 var updatebtn = '<button onclick="updateBook(\'' + id + '\',\'' + x + '\',\'' + y + '\')" class="button">Update</button>';
@@ -107,6 +108,13 @@ if(cnt>0){
     document.getElementById(x + "_report").style.display = "block";
 }
 });
+if($("#" + x + "books_only").val() != totalprice){
+    Swal.fire(
+  'NKPHS',
+  "Package price is different from booklist. Total Price according to booklist is(Books Only) :" + totalprice,
+  'info'
+);
+}
 if(x == "BENG"){
     document.getElementById("Bengnewbook").style.display = "block";
 }
@@ -115,6 +123,16 @@ else if(x == "HINDI"){
 }
 }
 
+function adminUpdate(){
+firebase.database().ref('SPDate').once('value').then((snapshot) => {
+var maxAllow = (snapshot.child("maxAllow").val());
+$("#maxallowed").val(maxAllow);
+});
+firebase.database().ref('item/charge').once('value').then((snapshot) => {
+var serviceCharge = (snapshot.child("service").val());
+$("#serviceCharge").val(serviceCharge);
+});
+}
 
 function slctupd(){
 var rootRef = firebase.database().ref('item/stationary/');
@@ -135,15 +153,79 @@ $("#rowscnt").text("Total No. of item added to Stationary = " + rowCount +"");
 }
 
 function updatemaxallow(){
+    Swal.fire({
+  title: 'Are you sure?',
+  text: "Number of students max allow will be updated.",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, Update'
+}).then((result) => {
+  if (result.isConfirmed) {
     firebase.database().ref("SPDate").update({maxAllow:document.getElementById("maxallowed").value});
+    Swal.fire(
+      'Updated!',
+      'Number of students max allow is updated.',
+      'success'
+    )
+  }
+})
+    
 }
 function updateServiceCharge(){
+     Swal.fire({
+  title: 'Are you sure?',
+  text: "Self Pickup with COD facility Service Charge will be updated.",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, Update'
+}).then((result) => {
+  if (result.isConfirmed) {
     firebase.database().ref("item/charge").update({service:document.getElementById("serviceCharge").value});
+    Swal.fire(
+      'Updated!',
+      'Service Charge updated.',
+      'success'
+    )
+  }
+})
+    
 }
 function updatefees() {
+    Swal.fire({
+  title: 'Are you sure?',
+  text: "Fees & Fine Charge for Class " + document.getElementById('clsslct').value + " will be updated.",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, Update'
+}).then((result) => {
+  if (result.isConfirmed) {
     firebase.database().ref("fees/" + document.getElementById("clsslct").value).update({fee:document.getElementById("fee").value,fine:document.getElementById("fine").value});
+    Swal.fire(
+      'Updated!',
+      'Fees updated.',
+      'success'
+    )
+  }
+});
+    
 }
 function bengPackage() {
+    Swal.fire({
+  title: 'Are you sure?',
+  text: "Package Details including price, Stock & Delivery Charge will be updated for Class: " + document.getElementById("clsslct").value + "and Second Language :Bengali",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, Update'
+}).then((result) => {
+  if (result.isConfirmed) {
     var checkBox = document.getElementById("BENGready");
     if (checkBox.checked == true){
     ready = 1;
@@ -151,8 +233,26 @@ function bengPackage() {
     ready = 0;
     }
     firebase.database().ref("item/package/BENG/" + document.getElementById("clsslct").value).update({books_bag:document.getElementById("BENGbooks_bag").value,books_only:document.getElementById("BENGbooks_only").value,deliveryCharge:document.getElementById("BENGdeliveryCharge").value,stock:document.getElementById("BENGstock").value,ready:ready});
+    Swal.fire(
+      'Updated!',
+      'Package Details Updated.',
+      'success'
+    )
+  }
+});
+
 }
 function hindiPackage() {
+    Swal.fire({
+  title: 'Are you sure?',
+  text: "Package Details including price, Stock & Delivery Charge will be updated for Class: " + document.getElementById("clsslct").value + "and Second Language :Hindi",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, Update'
+}).then((result) => {
+  if (result.isConfirmed) {
     var checkBox = document.getElementById("HINDIready");
     if (checkBox.checked == true){
     ready = 1;
@@ -160,36 +260,145 @@ function hindiPackage() {
     ready = 0;
     }
     firebase.database().ref("item/package/HINDI/" + document.getElementById("clsslct").value).update({books_bag:document.getElementById("HINDIbooks_bag").value,books_only:document.getElementById("HINDIbooks_only").value,deliveryCharge:document.getElementById("HINDIdeliveryCharge").value,stock:document.getElementById("HINDIstock").value,ready:ready});
+
+    Swal.fire(
+      'Updated!',
+      'Package Details Updated.',
+      'success'
+    )
+  }
+});
 }
 function addStationary() {
+Swal.fire({
+  title: 'Are you sure?',
+  text: "New item will be added to Stationary List.",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, Update'
+}).then((result) => {
+  if (result.isConfirmed) {
     var currTime = new Date().getTime();
     firebase.database().ref("item/stationary/" + currTime).update({id:currTime,name:document.getElementById("itemName").value,price:document.getElementById("itemPrice").value,remarks:document.getElementById("itemRemarks").value,stock:document.getElementById("itemStock").value});
+    Swal.fire(
+      'Updated!',
+      'New Item Added.',
+      'success'
+    )
+  }
+});
+    
 }
 function removeStationary(x) {
+            Swal.fire({
+  title: 'Are you sure?',
+  text: "Selected item will be removed from Stationary List.",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, Remove'
+}).then((result) => {
+  if (result.isConfirmed) {
     firebase.database().ref("item/stationary/" + x).remove();
     document.getElementById(x).style.display = "none";
+    Swal.fire(
+      'Deleted!',
+      'Item Removed.',
+      'success'
+    )
+  }
+});
+    
 }
 function updateStock(x){
+Swal.fire({
+  title: 'Are you sure?',
+  text: "Stock will be updated.",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, Update'
+}).then((result) => {
+  if (result.isConfirmed) {
     firebase.database().ref("item/stationary/" + x).update({stock:document.getElementById("stock" + x).value});
+    Swal.fire(
+      'Updated!',
+      'Stock Updated.',
+      'success'
+    )
+  }
+});
+    
 }
 
 function updateBook(x,y,z){
+    Swal.fire({
+  title: 'Are you sure?',
+  text: "Book Details of Class " + z +" - Second Language " + y + " will be updated.",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, Update'
+}).then((result) => {
+  if (result.isConfirmed) {
     firebase.database().ref("item/package/" + y + "/" + z + "/books/" + x).update({price:document.getElementById("price" + x + "splt" + y + "splt" + z).value,name:document.getElementById("name" + x + "splt" + y + "splt" + z).value,sub:document.getElementById("sub" + x + "splt" + y + "splt" + z).value,quantity:document.getElementById("quantity" + x + "splt" + y + "splt" + z).value});
+    Swal.fire(
+      'Updated!',
+      'Book Details Updated.',
+      'success'
+    )
+  }
+});
 }
 function removeBook(x,y,z) {
+Swal.fire({
+  title: 'Are you sure?',
+  text: "Book will be removed.",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, Remove'
+}).then((result) => {
+  if (result.isConfirmed) {
     firebase.database().ref("item/package/" + y + "/" + z + "/books/" + x).remove();
     document.getElementById("package" + x + y).style.display = "none";
-}
-
-function shopchange(){
-  var newst = Number(1 - Number(oldst)); 
-  console.log(newst);
-  document.getElementById(x.id).setAttribute("data-val", newst);
-  firebase.database().ref(mctype + "/" + shid).update({shopstatus:newst});
-  updateallitems(shid,newst);
+    Swal.fire(
+      'Removed!',
+      'Book removed.',
+      'success'
+    )
+  }
+});
+    
 }
 
 function addNewBook(x){
-    var y = document.getElementById("clsslct").value;
+var y = document.getElementById("clsslct").value;
+Swal.fire({
+  title: 'Are you sure?',
+  text: "New book will be added to class: " + y + " Second Language: " + x + "category of Book List.",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, Update'
+}).then((result) => {
+  if (result.isConfirmed) {
+
     firebase.database().ref("item/package/" + x + "/" + y + "/books/" + document.getElementById(x + "newbookid").value).update({id:document.getElementById(x + "newbookid").value,price:document.getElementById(x + "newbookprice").value,name:document.getElementById(x + "newbookname").value,sub:document.getElementById(x + "newbooksub").value,quantity:document.getElementById(x + "newbookquantity").value});
+    Swal.fire(
+      'Updated!',
+      'New Item Added.',
+      'success'
+    )
+  }
+});
+
+    
 }
