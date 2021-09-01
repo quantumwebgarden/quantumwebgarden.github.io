3b2cef40382eb6093b129fd02db8c16b11f97c5b
Quantum Webgarden <58441029+quantumwebgarden@users.noreply.github.com>
2021-09-01 09:43:52 +0530

    Updated

--- dot/doturation.js
+++ dot/doturation.js
@@ -1,24 +1,24 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyA7wly3NvCF90pJFs7a9kMBl-eXHJvXYw4",
-    authDomain: "dotqwdtls.firebaseapp.com",
-    databaseURL: "https://dotqwdtls.firebaseio.com",
-    projectId: "dotqwdtls",
-    storageBucket: "dotqwdtls.appspot.com",
-    messagingSenderId: "374863944922",
-    appId: "1:374863944922:web:3025e1648144afe89fd9e3",
-    measurementId: "G-NWB5LL551K"
-  };
-
 // var firebaseConfig = {
-//     apiKey: "AIzaSyDYKW6D_NZr_F8kQwWtUkz5G7RZavwUrtg",
-//     authDomain: "dotqwbackup1.firebaseapp.com",
-//     databaseURL: "https://dotqwbackup1.firebaseio.com",
-//     projectId: "dotqwbackup1",
-//     storageBucket: "dotqwbackup1.appspot.com",
-//     messagingSenderId: "650964876293",
-//     appId: "1:650964876293:web:04e0eae6fd1bd681e7706c",
-//     measurementId: "G-0QZG8962ZP"
+//     apiKey: "AIzaSyA7wly3NvCF90pJFs7a9kMBl-eXHJvXYw4",
+//     authDomain: "dotqwdtls.firebaseapp.com",
+//     databaseURL: "https://dotqwdtls.firebaseio.com",
+//     projectId: "dotqwdtls",
+//     storageBucket: "dotqwdtls.appspot.com",
+//     messagingSenderId: "374863944922",
+//     appId: "1:374863944922:web:3025e1648144afe89fd9e3",
+//     measurementId: "G-NWB5LL551K"
 //   };
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDYKW6D_NZr_F8kQwWtUkz5G7RZavwUrtg",
+    authDomain: "dotqwbackup1.firebaseapp.com",
+    databaseURL: "https://dotqwbackup1.firebaseio.com",
+    projectId: "dotqwbackup1",
+    storageBucket: "dotqwbackup1.appspot.com",
+    messagingSenderId: "650964876293",
+    appId: "1:650964876293:web:04e0eae6fd1bd681e7706c",
+    measurementId: "G-0QZG8962ZP"
+  };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
