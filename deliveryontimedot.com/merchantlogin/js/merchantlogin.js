129f1de80b5684fb22ab3dd27df40ced641ea06c
Quantum Webgarden <58441029+quantumwebgarden@users.noreply.github.com>
2022-03-06 19:02:35 +0530

    Updated

--- deliveryontimedot.com/merchantlogin/js/merchantlogin.js
+++ deliveryontimedot.com/merchantlogin/js/merchantlogin.js
@@ -1,16 +1,16 @@
 
 var flag = 0 ;
-var firebaseConfig = {
-    apiKey: "AIzaSyA7wly3NvCF90pJFs7a9kMBl-eXHJvXYw4",
-    authDomain: "dotqwdtls.firebaseapp.com",
-    databaseURL: "https://dotqwdtls.firebaseio.com",
-    projectId: "dotqwdtls",
-    storageBucket: "dotqwdtls.appspot.com",
-    messagingSenderId: "374863944922",
-    appId: "1:374863944922:web:3025e1648144afe89fd9e3",
-    measurementId: "G-NWB5LL551K"
-  };
-  firebase.initializeApp(firebaseConfig);
+// var firebaseConfig = {
+//     apiKey: "AIzaSyA7wly3NvCF90pJFs7a9kMBl-eXHJvXYw4",
+//     authDomain: "dotqwdtls.firebaseapp.com",
+//     databaseURL: "https://dotqwdtls.firebaseio.com",
+//     projectId: "dotqwdtls",
+//     storageBucket: "dotqwdtls.appspot.com",
+//     messagingSenderId: "374863944922",
+//     appId: "1:374863944922:web:3025e1648144afe89fd9e3",
+//     measurementId: "G-NWB5LL551K"
+//   };
+//   firebase.initializeApp(firebaseConfig);
 
 
 function loginmerchant() {
