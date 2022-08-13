6a2977884da64dbbda8b6e7e72a1a07ce939a979
Quantum Webgarden <58441029+quantumwebgarden@users.noreply.github.com>
2022-08-13 22:18:10 +0530

    updated

--- prasonno/monginis.js
+++ prasonno/monginis.js
@@ -51,3 +51,25 @@ var removeLine = "<button class='rmv' onclick='removeOrder(" + orderId + ")'>Rem
       alert("Error");
     }
   }
+
+  function removeOrder(x){
+  Swal.fire({
+  title: 'Are you sure?',
+  text: "You won't be able to revert this!",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, delete it!'
+}).then((result) => {
+  if (result.isConfirmed) {
+    firebase.database().ref("orders/" + x).remove();
+    Swal.fire(
+      'Deleted!',
+      'Your product has been deleted.',
+      'success'
+    )
+    location.reload();
+  }
+})
+}
